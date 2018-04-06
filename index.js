790816ecd78bf3761b8624d8fecd64d37f2dafd8
xLonz <37774889+xLonz@users.noreply.github.com>
2018-04-06 21:13:04 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+let cooldown = new Set();
+let cdseconds = 86400;
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("*present for Attendance", {type: "WATCHING"});
+  
+});
+
+bot.on("message", async message => {
+if(message.author.bot) return;
+if(message.channel.type === "dm") return;
+  
+let prefix = botconfig.prefix;
+  
+  if(!message.content.startsWith(prefix)) return;
+let messageArray = message.content.split(" ");
+let cmd = messageArray[0];
+let args =  messageArray.slice(1);
+    
+ if(cmd === `${prefix}present`){
+    
+   if(cooldown.has(message.author.id)){
+    message.delete();
+  return message.reply("You have to wait 1 day.")
+  }
+  cooldown.add(message.author.id);
+  let attendanceEmbed = new Discord.RichEmbed()
+  .setDescription("Attendance")
+  .setColor("#15f153")
+  .addField("Member Present", `${message.author}`)
+  .addField("Time", message.createdAt)
+  
+  let attendancechannel = message.guild.channels.find(`name`, "dn-attendance");
+  if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
+  
+  
+  message.delete().catch(O_o=>{});
+  attendancechannel.send(attendanceEmbed);
+   
+   setTimeout(() => {
+      cooldown.delete(message.author.id)
+      }, cdseconds * 1000)
+
+    
+  }
+  
+  if(cmd === `${prefix}botinfo`){
+   
+  let bicon = bot.user.displayAvatarURL;
+  let botembed = new Discord.RichEmbed()
+  .setDescription("Bot Information")
+  .setColor("#15f153")
+  .setThumbnail(bicon)
+  .addField("Bot Name", bot.user.username);
+   
+  return message.channel.send(botembed);
+}
+    
+});
+
+bot.login(process.env.BOT_TOKEN);
