d5b65b5b23a2fbaf81299b629b2dd5ded8e671ce
xLonz <37774889+xLonz@users.noreply.github.com>
2018-04-14 21:04:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let args =  messageArray.slice(1);
   .setDescription("Attendance")
   .setColor("#15f153")
   .addField("Member Present", `${message.author}`)
-  .addField("Time", message.createdAt)
+  .addField("Time", message.createdTimestamp)
   
   let attendancechannel = message.guild.channels.find(`name`, "dn-attendance");
   if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
