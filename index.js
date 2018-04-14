bd39cfdb8000253b4ed4e80b5ac8afbaa157474b
xLonz <37774889+xLonz@users.noreply.github.com>
2018-04-14 16:47:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let args =  messageArray.slice(1);
   let attendanceEmbed = new Discord.RichEmbed()
   .setDescription("Attendance")
   .setColor("#15f153")
-  .addField("Member Present", `${message.author}`)
+  .addField("Member Present", `${message.author.id}`)
   .addField("Time", message.createdAt)
   
   let attendancechannel = message.guild.channels.find(`name`, "dn-attendance");
