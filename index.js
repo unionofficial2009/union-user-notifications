d3f5629a88cedecc4e32d9090b1a5559d5c3a112
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-17 11:24:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 
+let userStatus = [];
+bot.on("presenceUpdate", (oldMember, newMember) => {
+    let username = newMember.user.username;
+    let status = newMember.user.presence.status;
+  
+    userStatus.push(username, status);
+    //console.log(`${newMember.user.username} is now ${newMember.user.presence.status}`);
+    guildChannels.find('name', 'general')
+        .send(`${newMember.user.username} is now ${newMember.user.presence.status}`)
+        .then(msg => {
+            // do something else if you want
+        })
+        .catch(console.error)
+})
+
+
+
+
 bot.login(process.env.BOT_TOKEN);
