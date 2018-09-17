a003fd4cb7c1e3e5b7beeffaf2e06c6c52fdfab0
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-17 12:34:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
   
     userStatus.push(username, status);
     guildChannels.find('name', 'user-status')
-        .send(`<@${newMember.user.id}> is now ${newMember.user.status}`)
+        .send(`<@${newMember.user.id}> is now ${newMember.user.presence.status}`)
         .then(msg => {
             // do something else if you want
         })
