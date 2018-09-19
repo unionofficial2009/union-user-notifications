a09da2abd6b8c814db6af78708327d4c60e96018
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-19 13:53:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ bot.on('guildMemberAdd', member => {
    let bicon2 = bot.user.displayAvatarURL;  
    let botembed = new Discord.RichEmbed()
         .setDescription("Hello maam/sir welcome to UNION.")
-        .addField("Instructions", "Please proceed to **#welcome** channel on the **UNION HEADQUATERS** discord server. Kindly read and follow the rules/instructions on the **welcome** channel to access the channels for specific game.")
+        .addField("Instructions", "Please proceed to **#welcome** channel on the **UNION HEADQUATERS** discord server. Kindly read and follow the rules/instructions on the **#welcome** channel to access the channels for specific game.")
         .addField("Need Help?", "Any problem, please DM **@UNION President  @UNION Vice President @UNION Officers @UNION Human Resources**.")
         .setColor("#15f153")
         .setTimestamp()
