3794aa5556c3ebbbf5ad55ee17978278f996dc29
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-19 13:31:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,60 +2,8 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 
-let userStatus = [];
-bot.on("presenceUpdate", (oldMember, newMember) => {
-    let username = newMember.user.username;
-    let status = newMember.user.presence.status;
-    // get a reference to all channels in the user's guild
-    let guildChannels = newMember.guild.channels;
-    
-    
-  let bicon = newMember.user.displayAvatarURL;
-  let bicon2 = bot.user.displayAvatarURL;  
-  
-    
-    
-    if(oldMember.presence.status == newMember.presence.status && newMember.presence.status == "offline"){
-        
-       
-        
-        let botembed = new Discord.RichEmbed()
-        .setDescription(`<@${newMember.user.id}>`)
-        .addField("Username", `${newMember.user.username}`)
-        .setColor("#15f153")
-        .setThumbnail(bicon)
-        .addField("Status", `${newMember.user.presence.status} (invisible)`)
-        .setTimestamp()
-        .setFooter("UNION User Status",bicon2);
-        
-        userStatus.push(username, status);
-        guildChannels.find('name', 'user-status')
-        .send(botembed)
-        .then(msg => {
-            // do something else if you want
-        })
-        .catch(console.error)
-   
-    } else {
-  
-    let botembed = new Discord.RichEmbed()
-    .setDescription(`<@${newMember.user.id}>`)
-    .addField("Username", `${newMember.user.username}`)
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Status", `${newMember.user.presence.status}`)
-    .setTimestamp()
-    .setFooter("UNION User Status",bicon2);    
-        
-    userStatus.push(username, status);
-    guildChannels.find('name', 'user-status')
-        .send(botembed)
-        .then(msg => {
-            // do something else if you want
-        })
-        .catch(console.error)
-        
-    }
+bot.on('guildMemberAdd', member => {
+   member.send("Welcome to the server!");
 });
   
 
